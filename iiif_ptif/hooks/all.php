390911284c2bf1b2bedf5bf6d311ca8a44b45ce1
Michiel <michiel@herosolutions.be>
2019-11-28 11:15:07 +0100

    Adds clickable links to IIIF manifests

--- iiif_ptif/hooks/all.php
+++ iiif_ptif/hooks/all.php
@@ -180,6 +180,33 @@
                 executeImagehubCommands($ref);
             }
         }
+    }
 
+    # Renders clickable URL's to IIIF viewers above the preview image when opening a resource
+    # Configure the $iiif_ptif_viewers field in config.php to generate appropriate URL's
+    function HookIiif_ptifAllRenderbeforeresourceview($resource)
+    {
+        global $iiif_imagehub_manifest_url, $iiif_imagehub_viewers;
+
+        if(isset($iiif_imagehub_manifest_url) && isset($iiif_imagehub_viewers)) {
+            $url = str_replace('{ref}', $resource['ref'], $iiif_imagehub_manifest_url);
+            $handle = curl_init($url);
+            curl_setopt($handle, CURLOPT_SSL_VERIFYHOST, 0);
+            curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, 0);
+            curl_setopt($handle, CURLOPT_NOBODY, true);
+            $response = curl_exec($handle);
+            $httpCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+            curl_close($handle);
+            if($httpCode == 200) {
+                foreach($iiif_imagehub_viewers as $key => $viewer) {
+                    $url = str_replace('{manifest_url}', urlencode($url), $viewer);
+                    echo '<p><a href=' . $url . '>View in ' . $key . '</a></p>';
+                }
+            } else {
+                foreach($iiif_imagehub_viewers as $key => $viewer) {
+                    echo '<p>There is currently no working link to ' . $key . ' yet.</p>';
+                }
+            }
+        }
     }
 ?>
